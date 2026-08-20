61f26b7164271f2a47bf6c92b78ee04a4c028bcf
Lee seung hyuck <fasteel4@naver.com>
2026-08-20 23:33:03 +0900

    Delete ADV_FakeReview directory

--- ADV_FakeReview/scripts/download_yelpchi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# YelpChi (Rayana & Akoglu 2015, CARE-GNN 전처리본) 다운로드
-set -e
-TMP_DIR=$(mktemp -d)
-git clone --depth 1 https://github.com/YingtongDou/CARE-GNN.git "$TMP_DIR/caregnn"
-mkdir -p "$(dirname "$0")/../data/raw/yelpchi"
-unzip -o "$TMP_DIR/caregnn/data/YelpChi.zip" -d "$(dirname "$0")/../data/raw/yelpchi"
-rm -rf "$TMP_DIR"
-echo "Done. See data/raw/yelpchi/YelpChi.mat"
